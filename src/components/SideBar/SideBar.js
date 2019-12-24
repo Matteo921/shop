feedc429d73a6da23df7a65aa3de8adbf68e30a1
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-12-24 14:59:43 +0100

    correct styles

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -14,11 +14,11 @@ export default class SideBar extends React.Component {
     return (
         <div className="clothes-list">
             <NavLink exact to="/" activeClassName="active" className="clothes">Sport clothes</NavLink> 
-            <NavLink exact to="/" activeClassName="active" className="clothes">Accessiories</NavLink> {"//"}
+            <NavLink exact to="/" activeClassName="active" className="clothes">Accessiories</NavLink> 
             <NavLink exact to="/" activeClassName="active" className="clothes">Summer clothes</NavLink>
-            <NavLink exact to="/" activeClassName="active" className="clothes">Elegance clothes</NavLink> {"//"}
+            <NavLink exact to="/" activeClassName="active" className="clothes">Elegance clothes</NavLink> 
            <NavLink exact to="/" activeClassName="active" className="clothes">Casual clothes</NavLink>
-           <NavLink exact to="/" activeClassName="active" className="clothes">Outlet</NavLink> {"//"}
+           <NavLink exact to="/" activeClassName="active" className="clothes">Outlet</NavLink> 
            <NavLink exact to="/" activeClassName="active" className="clothes reset">Reset Filters</NavLink>
 
         </div>
