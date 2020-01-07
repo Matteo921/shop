a34fc506998c06c8299fcf5bd66903eea60cff02
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2020-01-07 23:15:03 +0100

    Add home style

--- user/src/components/pages/Home/HomePage.js
+++ user/src/components/pages/Home/HomePage.js
@@ -8,12 +8,12 @@ class HomePage extends React.Component {
     return (
       <div className="home">
         <div className="home-sort col-sm-12 col-md-4 col-lg-3">
-          <p className="home-sort-header">Sort by:</p>
+          <p className="home-sort-header">Sortuj:</p>
           <ul className="home-sort-list">
             <li>Name: A-Z</li>
             <li>Name: Z-A</li>
-            <li>Price: lowest first</li>
-            <li>Price: highest first</li>
+            <li>Cena rosnąco</li>
+            <li>Cena malejąco</li>
           </ul>
         </div>
         <div className="products">
