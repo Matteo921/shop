877fcd4b5e56ac46f594477c58d9719e33cc5b7a
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2020-01-12 22:24:24 +0100

    add FAQ,icons

--- user/src/App.js
+++ user/src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/pages/Home/HomePage';
-
-import FAQ from './components/pages/Faq/FAQ';
-import Regulamin from './components/pages/Regulamin/RegulaminPage';
+import FAQ from './components/pages/Faq/FAQpage';
+import Regulamin from './components/pages/Terms/TermsPage';
 import Contact from './components/pages/Contact/ContactPage';
 import ProductCard from './components/features/ProductCard/ProductCard';
 import NotFound from './components/pages/NotFound/NotFoundPage';
